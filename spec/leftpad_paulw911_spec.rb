199766d0f9c900a1ec914c5d43f1a8c32e90f51c
paulw911 <paulw911@netspace.net.au>
2017-09-28 13:44:28 +1000

    new minor version - added tests to spec file

--- spec/leftpad_paulw911_spec.rb
+++ spec/leftpad_paulw911_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe LeftpadPaulw911 do
     expect(LeftpadPaulw911::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "leftpad_paulw911 with no padding char" do
+    expect('left'.leftpad_paulw911(8)).to eq('    left')
   end
+
+  it "leftpad_paulw911 with padding char" do
+    expect('left'.leftpad_paulw911(8, '0')).to eq('0000left')
+  end
+
+  it "leftpad_paulw911 with fewer padding char" do
+    expect('left'.leftpad_paulw911(2, '0')).to eq('left')
+  end
+
 end
